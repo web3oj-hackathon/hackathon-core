f076b120c7c1f11232fb6f5910d753b1da1e2ac9
0xall <ksw3894@gmail.com>
2023-05-19 22:41:05 +0900

    add permit function to dev erc20

--- contracts/test/DevERC20.sol
+++ contracts/test/DevERC20.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ERC20 for testing
  * @notice this contract is for testing.
  */
-contract DevERC20 is ERC20, Ownable {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+contract DevERC20 is ERC20, ERC20Permit, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) ERC20Permit(name) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
