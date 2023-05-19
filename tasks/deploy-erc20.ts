5722aecf08740e1581421dfdd2b6e89d9ebb39b9
0xall <ksw3894@gmail.com>
2023-05-19 22:59:33 +0900

    add mint task

--- tasks/deploy-erc20.ts
+++ tasks/deploy-erc20.ts
@@ -6,9 +6,26 @@ task("deploy-erc20", "Deploy ERC20 contract")
   .setAction(async (args, hre) => {
     const { name, symbol } = args;
 
-    const DevERC20 = await ethers.getContractFactory("DevERC20");
+    const DevERC20 = await hre.ethers.getContractFactory("DevERC20");
     const devERC20 = await DevERC20.deploy(name, symbol);
     await devERC20.deployed();
 
     console.log(`${name}(${symbol}) token deployed to:`, devERC20.address);
+  });
+
+task("mint", "Mint ERC20 tokens")
+  .addParam("contract")
+  .addParam("amount")
+  .setAction(async (args, hre) => {
+    const { contract, amount } = args;
+
+    const DevERC20 = await hre.ethers.getContractFactory("DevERC20");
+    const devERC20 = await DevERC20.attach(contract);
+
+    const account = await hre.ethers.provider.getSigner().getAddress();
+
+    const tx = await devERC20.mint(account, hre.ethers.utils.parseEther(amount));
+    await tx.wait();
+
+    console.log(`${amount} tokens minted to:`, account);
   });
